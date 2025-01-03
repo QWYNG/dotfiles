236129aa12b7ead6bada7d35cca53ae633ff36f4
qwyng <ikusawasi@gmail.com>
2025-01-03 11:09:56 +0900

    init configs

---
+++ fish/config.fish
@@ -0,0 +1,26 @@
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+end
+
+alias gpm='git pull --rebase origin master'
+alias gcf 'git branch | grep -v "master" | xargs git branch -D'
+alias gl='git log -p --name-only'
+
+set PATH ~/.cargo/bin/ $PATH
+set PATH /opt/homebrew/bin $PATH
+
+alias be='bundle exec'
+alias bi='bundle install'
+alias ls='exa -lh --group-directories-first --icons'
+
+set EDITOR 'emacs -nw'
+set PATH ~/.cargo/bin/ $PATH
+set PATH /Users/qwyng/Library/Application\ Support/JetBrains/Toolbox/scripts/ $PATH
+set PATH /opt/homebrew/bin $PATH
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/qwyng/google-cloud-sdk/path.fish.inc' ]; . '/Users/qwyng/google-cloud-sdk/path.fish.inc'; end
+if status is-interactive
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+~/.local/bin/mise activate fish | source
